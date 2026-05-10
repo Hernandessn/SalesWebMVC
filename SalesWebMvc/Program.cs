484aac7173d9b16d404b32fa82e3d8dd56528a5b
hernandessn <hernandessalesneves7@gmail.com>
2026-05-10 08:58:25 -0300

    feat(sellers): add SellerService with basic FindAll method

--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Data;
+using SalesWebMvc.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,7 @@ builder.Services.AddDbContext<SalesWebMvcContext>(options =>
     ));
 
 builder.Services.AddScoped<SeedingService>();
+builder.Services.AddScoped<SellerService>();
 
 builder.Services.AddControllersWithViews();
 
