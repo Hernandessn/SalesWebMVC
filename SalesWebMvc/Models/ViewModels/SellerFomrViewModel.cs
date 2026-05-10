437aa419d0ceab54d2207245ab1fef73c612fc67
hernandessn <hernandessalesneves7@gmail.com>
2026-05-10 09:51:44 -0300

    feat(sellers): add SellerFormViewModel, DepartmentService and department select on create form

---
+++ SalesWebMvc/Models/ViewModels/SellerFomrViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SalesWebMvc.Models.ViewModels
+{
+    public class SellerFomrViewModel
+    {
+        public Seller Seller { get; set; }
+        public ICollection<Department> Departments { get; set; }
+
+    }
+}
--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddDbContext<SalesWebMvcContext>(options =>
 
 builder.Services.AddScoped<SeedingService>();
 builder.Services.AddScoped<SellerService>();
+builder.Services.AddScoped<DepartmentService>();
 
 builder.Services.AddControllersWithViews();
 
